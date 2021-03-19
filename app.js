b37525d5b1e21e4fc9ce307e7238e85ed12e947b
Robert <rbbenett0614@gmail.com>
2021-03-19 11:22:59 -0400

    Updated index to generate dynamic content

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const items = [
+    { name: 'mobile phone', price: 1000 },
+    { name: 'book', price: 30 },
+    { name: 'computer', price: 2000 },
+  ]
+  res.render('index', { items })
 })
 app.get('/add-item', (req, res) => {
   res.render('add-item')

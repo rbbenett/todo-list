f207429238dd1864e6c2b62d8bf762ee7c856eff
Robert <rbbenett0614@gmail.com>
2021-03-19 10:12:06 -0400

    Added routes

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.send('<p>Home Page</p>')
+  res.sendFile('./views/index.html', {root: __dirname})
+})
+app.get('/add-item', (req, res) => {
+  res.sendFile('./views/add-item.html', {root: __dirname})
+})
+app.use((req, res) => {
+  res.sendFile('./views/error.html', {root: __dirname})
 })

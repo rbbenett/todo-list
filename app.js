2e1df6be6bb7a2c318fc60f02aba9ef244b0dbf2
Robert <rbbenett0614@gmail.com>
2021-03-19 11:08:43 -0400

    Refactored with ejs

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', {root: __dirname})
+  res.render('index')
 })
 app.get('/add-item', (req, res) => {
-  res.sendFile('./views/add-item.html', {root: __dirname})
+  res.render('add-item')
 })
 app.use((req, res) => {
-  res.sendFile('./views/error.html', {root: __dirname})
+  res.render('error')
 })

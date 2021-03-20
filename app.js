00fa3cbdf914b8069fc2d05e412fc84b1d0fc341
Robert <rbbenett0614@gmail.com>
2021-03-20 14:12:07 -0400

    Render data from mongoDB

--- app.js
+++ app.js
@@ -17,32 +17,21 @@ mongoose.connect(mongodb, {
 app.set('view engine', 'ejs');
 
 
-app.get('/create-item', (req, res) => {
-  const item = new Item({
-    name: 'computer',
-    price: 2000
-  });
-  item.save().then(result=>res.send(result))
-})
-app.get('/get-items', (req, res) => {
-  
-  Item.find().then(result=>res.send(result)).catch((err)=>console.log(err))
-})
-app.get('/get-item', (req, res) => {
-  
-  Item.findById("6054ea245d57128bcb88d39c").then(result=>res.send(result)).catch((err)=>console.log(err))
-})
 app.get('/', (req, res) => {
-  const items = [
-    { name: 'mobile phone', price: 1000 },
-    { name: 'book', price: 30 },
-    { name: 'computer', price: 2000 },
-  ]
-  res.render('index', { items })
+  res.redirect('/get-items');
+})
+
+app.get('/get-items', (req, res) => {
+
+  Item.find().then(result => {
+
+      res.render('index', { items: result });
+  }).catch(err => console.log(err))
 })
 app.get('/add-item', (req, res) => {
-  res.render('add-item')
+  res.render('add-item');
 })
+
 app.use((req, res) => {
-  res.render('error')
+  res.render('error');
 })
